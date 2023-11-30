d4d7d03d70f09593730c3494cb3f57dcf496997c
Mohamed Samb <mohameds@mit.edu>
2023-11-30 11:02:09 -0500

    Fixed restrictions store

--- client/stores/restrictions.ts
+++ client/stores/restrictions.ts
@@ -1,25 +1,29 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-export const useRestrictionStore = defineStore("user", () => {
-  const restrictions = ref([
-    "Vegetarian",
-    "Vegan",
-    "Pescetarian",
-    "Gluten-Free",
-    "Lactose-Free",
-    "Nut-Free",
-    "Shellfish-Free",
-    "Soy-Free",
-    "Egg-Free",
-    "Halal",
-    "Kosher",
-    "Wheat-Free",
-    "Fish-Free",
-    "Other",
-  ]);
+export const useRestrictionStore = defineStore(
+  "restriction",
+  () => {
+    const restrictions = ref([
+      "Vegetarian",
+      "Vegan",
+      "Pescetarian",
+      "Gluten-Free",
+      "Lactose-Free",
+      "Nut-Free",
+      "Shellfish-Free",
+      "Soy-Free",
+      "Egg-Free",
+      "Halal",
+      "Kosher",
+      "Wheat-Free",
+      "Fish-Free",
+      "Other",
+    ]);
 
-  return {
-    restrictions,
-  };
-});
+    return {
+      restrictions,
+    };
+  },
+  { persist: true },
+);
