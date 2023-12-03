be2edec6fd7ecb26c50d0772d032b8f3052f5b26
Mohamed Samb <mohameds@mit.edu>
2023-12-03 18:04:28 -0500

    Updated styling of feedback state form

--- client/components/Feedback State/FeedbackStateForm.vue
+++ client/components/Feedback State/FeedbackStateForm.vue
@@ -1,43 +1,77 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const rating = ref(1);
+const rating = ref(0);
 const effectiveness = ref(0);
+const stars = ref([1, 2, 3, 4, 5])
+const hover = ref(0)
+const props = defineProps(['response'])
 
 
-const updateRating = async () => {
+const updateRating = async (star: number) => {
     // check if there already exists a rating
     // If not, create a new rating
     // If so, update the rating
+    rating.value = star;
+}
+
+const hoverRating = async (star:number) => {
+    hover.value = star
+}
+
+const resetHover = async () => {
+    hover.value = 0
 }
 
 const getEffectiveness = async () => {
     //route for getting rating
+    // get average rating
 }
 </script>
 
 <template>
     <div>
-        <i>Effectiveness: {{ effectiveness }}</i>
+        <p>Response: {{ props.response.explanation}}</p>
     </div>
-    <label class="rating-label">
-        <strong>Your Rating: </strong>
-        <input
-            class="rating"
-            max="5"
-            oninput="this.style.setProperty('--value', this.value)"
-            step="0.5"
-            type="range"
-            v-model="rating"
-            @click="updateRating">
-    </label>
+    <div class="feedback-info">
+        <div>
+            <i>Effectiveness: {{ (effectiveness)? (effectiveness.toFixed(1)) : "-" }}</i>
+        </div>
+        <div class="rating">
+            <div>Your Rating: </div>
+            <span v-for="star in stars" :key="star" @click="updateRating(star)"
+                @mouseover="hoverRating(star)"
+                @mouseleave="resetHover"
+                :class="{ 'active': star <= rating || star <= hover }"></span>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-form {
-  display: flex;
-  gap: 0.5em;
-  padding: 1em;
-  align-items: center;
+.feedback-info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.rating {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.rating span {
+    cursor: pointer;
+    width: 40px;
+    height: 40px;
+    background-image: url('../../assets/images/black-star.png'); /* Replace with your star image */
+    background-size: cover;
+    margin-right: 5px;
+}
+
+.rating span:hover,
+.rating span.active,
+.rating span.active:hover {
+    background-image: url('../../assets/images/gold-star.png'); /* Replace with your filled star image */
 }
 </style>
