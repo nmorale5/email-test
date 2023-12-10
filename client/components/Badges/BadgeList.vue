d2bf4c3d988a007972605574752a35e73fefca34
Kevin Lam <lamk5977@gmail.com>
2023-12-10 17:12:43 -0500

    Changed filepath to images test

--- client/components/Badges/BadgeList.vue
+++ client/components/Badges/BadgeList.vue
@@ -9,7 +9,7 @@ const getBadgeIconFile = (badgeName: string) => {
   if (idx === -1) {
     return;
   }
-  return `/images/tile${idx.toString().padStart(3, "0")}.png`;
+  return `/tile${idx.toString().padStart(3, "0")}.png`;
 };
 </script>
 
