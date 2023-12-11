a87fc81d520ea3e0050e14a1b2c09943d761aeec
Kevin Lam <lamk5977@gmail.com>
2023-12-10 23:47:01 -0500

    Fixed bug with signing not triggering response email.

--- client/components/Petition/PetitionComponent.vue
+++ client/components/Petition/PetitionComponent.vue
@@ -33,6 +33,7 @@ const updateSigned = async () => {
     newSigned = currentUserId.value !== "" ? await fetchy(`/api/upvote/${props.petition._id}/${currentUserId.value}`, "GET") : false;
     newSigners = (await fetchy(`/api/upvote/${props.petition._id}`, "GET")).length;
   } catch {
+    console.log("Failed to update signed and signers")
     return;
   }
   signed.value = newSigned;
@@ -41,8 +42,9 @@ const updateSigned = async () => {
 
 const trySign = async () => {
   try {
-    await fetchy(`/api/upvote/${props.petition._id}/${currentUserId.value}`, "PUT");
+    await fetchy(`/api/petition/${props.petition._id}/${currentUserId.value}`, "PUT")
   } catch {
+    console.log("Failed to sign petition")
     return;
   } finally {
     await updateSigned();
